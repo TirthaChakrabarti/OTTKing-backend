0182e68cb9ab00a4c5b503689abc62c887aac44c
Tirtha Chakrabarti <chakrabartitirtha2@gmail.com>
2025-06-05 19:22:23 +0530

    fix: DB connection details changed

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(cors({origin: 
-    "*"
-}));
+app.use(cors({origin: "*" }));
 // app.all('/', function(req, res, next) {
 // res.header("Access-Control-Allow-Origin", "*");
 // res.header("Access-Control-Allow-Headers", "X-Requested-With");
--- config/db.js
+++ config/db.js
@@ -1,19 +1,19 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: process.env.HOST || 'localhost',
-    user: process.env.USER || 'root',
-    password: process.env.PASSWORD || '',
-    database: process.env.DATABASE || 'ottkingdevdb'
-})
+  host: process.env.HOST || "localhost",
+  user: process.env.USER || "root",
+  password: process.env.PASSWORD || "MyMySQL.123",
+  database: process.env.DATABASE || "ottkingdevdb",
+});
 
 db.connect((error) => {
-    if (error) {
-        console.log('Error connecting to the database');
-        process.exit(1);
-    }
+  if (error) {
+    console.log("Error connecting to the database", error);
+    process.exit(1);
+  }
 
-    console.log('Successfully connected to the MySQL database')
-})
+  console.log("Successfully connected to the MySQL database");
+});
 
-module.exports = db
+module.exports = db;
